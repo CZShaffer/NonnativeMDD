7cea6eff22bdfe8145c53b56b213229852b2c947
CZShaffer <pb10459@umbc.edu>
2021-10-17 19:00:27 -0400

    wrote scripts to download and extract necessary datasets. Updated gitignore to not commit any downloaded data

--- data/prep_data.py
+++ data/prep_data.py
@@ -1,5 +1,33 @@
-def prep_librispeech():
-    pass
+import os
+from url_retrieve import url_retrieve
 
-def prep_l2_arctic():
-    pass
+def prep_librispeech(dataset="mini", deleteExisting=True, compression="tar.gz"):
+    '''
+    Downloads and extracts the Librispeech dataset to the data folder
+    :param dataset: which Librispeech dataset to download, defaults to Librispeech mini. Must be one of "mini", "100", or "360"
+    :param deleteExisting: whether or not to delete file if it already exists, defaults to skip download if file exists
+    :param compression: compression type of the file to be extracted. Deletes compressed file after extracting.
+    Setting to none leaves the file compressed and will not delete the compressed file
+    :return:
+    '''
+    mini_train = "http://www.openslr.org/resources/31/train-clean-5.tar.gz"
+    libri_100_train = "http://www.openslr.org/resources/31/train-clean-100.tar.gz"
+    libri_360_train = "http://www.openslr.org/resources/31/train-clean-360.tar.gz"
+    if dataset == "mini":
+        url_retrieve(mini_train, dir="Librispeech/",deleteExisting=deleteExisting, compression=compression)
+    if dataset == "100":
+        url_retrieve(libri_100_train, dir="Librispeech/",deleteExisting=deleteExisting, compression=compression)
+    if dataset == "360":
+        url_retrieve(libri_360_train, dir="Librispeech/",deleteExisting=deleteExisting, compression=compression)
+
+def prep_l2_arctic(deleteExisting=True, compression="zip"):
+    '''
+    Downloads and extracts the L2 ARCTIC dataset to the data folder
+    :param deleteExisting: whether or not to delete file if it already exists, defaults to skip download if file exists
+    :param compression: compression type of the file to be extracted. Deletes compressed file after extracting.
+    Setting to none leaves the file compressed and will not delete the compressed file
+    :return:
+    '''
+    # Before accessing this data, please request permission from https://psi.engr.tamu.edu/l2-arctic-corpus/
+    l2_arctic = "https://drive.google.com/uc?id=1JvI3ktRJdC-CSHEieQMpS8Wca2FTPxjr"
+    url_retrieve(l2_arctic, dir="L2_ARCTIC/",filename="L2_ARCTIC" ,deleteExisting=deleteExisting, compression=compression, gdrive=True)
--- data/url_retrieve.py
+++ data/url_retrieve.py
@@ -1,28 +1,89 @@
-import urllib2
+import urllib
+import urllib.request
+import os
+from tqdm import tqdm
+import tarfile
+import zipfile
+import gdown
 
-def url_retireve(url, filename=None):
+def url_retrieve(url, dir=None, filename=None, deleteExisting=False, compression=None, gdrive=False):
+    '''
+    Downloads data from a given url
+    :param url: url of file to download
+    :param dir: directory where file will be written to, defaults to current directory
+    :param filename: filename of file being saved, defaults to name of retrieved file
+    :param deleteExisting: whether or not to delete file if it already exists, defaults to skip download if file exists
+    :param compression: compression type of the file to be extracted. Deletes compressed file after extracting.
+    Setting to none leaves the file compressed and will not delete the compressed file
+    :param gdrive: Whether or not the url links to google drive, defaults to false
+    :return:
+    '''
     file_name = url.split('/')[-1] if filename is None else filename
-    # taken from https://stackoverflow.com/questions/22676/how-to-download-a-file-over-http
-    u = urllib2.urlopen(url)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print
-    "Downloading: %s Bytes: %s" % (file_name, file_size)
+    if dir is not None:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        file_name = dir + file_name
+    if os.path.exists(file_name) or os.path.exists(file_name.split('.')[0]):
+        if not deleteExisting:
+            print(f"{file_name} already exists. Skipping Download.")
+            return
+        print(f"{file_name} already exists. Deleting existing file.")
 
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
+    if gdrive:
+        gdown.download(url, "L2_ARCTIC", quiet=False)
+    else:
+        # based on https://stackoverflow.com/questions/22676/how-to-download-a-file-over-http
+        f = open(file_name, 'wb')
+        with urllib.request.urlopen(url) as u:
+            file_size = int(u.getheader("Content-Length"))
+            print(f"Downloading: {file_name} Bytes: {file_size}")
+            progress_bar = tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024)
+            file_size_dl = 0
+            block_sz = 8192*4
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
+                file_size_dl += len(buffer)
+                f.write(buffer)
+                progress_bar.update(block_sz)
+            progress_bar.close()
+            print(file_size_dl)
+        f.close()
+        # end cite
+    print(f"downloaded {file_name}")
+    extract(file_name, compression)
+    print("Done")
 
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8) * (len(status) + 1)
-        print
-        status,
+def extract(file_name, compression):
+    '''
+    extracts files from a given archive to a directory of the same name
+    :param file_name: file path to the compressed file
+    :param compression: compression type of the file to be extracted. Deletes compressed file after extracting.
+    Setting to none leaves the file compressed and will not delete the compressed file
+    :return:
+    '''
+    if compression == "tar":
+        tar = tarfile.open(file_name, "r:")
+        print(f"Extracting to {file_name[:-4]}")
+        if not os.path.exists(file_name[:-4]):
+            os.makedirs(file_name[:-4])
+        tar.extractall(file_name[:-4])
+        tar.close()
+    elif compression == "tar.gz":
+        tar = tarfile.open(file_name, "r:gz")
+        print(f"Extracting to {file_name[:-7]}")
+        if not os.path.exists(file_name[:-7]):
+            os.makedirs(file_name[:-7])
+        tar.extractall(file_name[:-7])
+        tar.close()
+        os.remove(file_name)
+    elif compression == "zip":
+        zip = zipfile.ZipFile(file_name, "r:")
+        print(f"Extracting to {file_name[:-4]}")
+        if not os.path.exists(file_name[:-4]):
+            os.makedirs(file_name[:-4])
+        zip.extractall(file_name[:-4])
+        zip.close()
 
-    f.close()
-    #end cite
+
