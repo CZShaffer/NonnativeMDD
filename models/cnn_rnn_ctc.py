42ef9f26de6a9c1db7ba26e51e11bdf4bf63580a
xiaofanc <xfang.chu@gmail.com>
2021-11-30 14:18:01 -0800

    initiate CNN-RNN-CTC model

--- models/cnn_rnn_ctc.py
+++ models/cnn_rnn_ctc.py
@@ -4,7 +4,21 @@ import torch.nn as nn
 class cnn_rnn_ctc(nn.Module):
     def __init__(self):
         super(cnn_rnn_ctc, self).__init__()
-        self.full_stack = nn.Sequential()
+        # first part
+        # input layer received the framewise acoustic features, followed by a batch normalization + zero padding
+
+        # second part
+        # convolution, 4 CNN layers + 2 maxpooling layer + batch normalization
+
+        # third part
+        # bi-directional RNN - GRU
+
+        # fourth part
+        # time distributed dense layer - MLP + softmax
+
+        # last part
+        # CTC output layer
+
 
     def forward(self, x):
         pass
